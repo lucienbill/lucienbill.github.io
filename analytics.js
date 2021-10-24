1b7fc695b994807502bf34de58b7a4d18d0f9826
Lucien Bill <lucien.bill@gmail.com>
2021-10-24 17:29:26 +0200

    Revert "Analytics -> async (js)"
    This reverts commit 8d1d58746ebbdf504c43e6e1c70838e2958feefc.
    
    Mon commit était foireux : les variables de Jekyll Now ne sont pas interpretées dans le fichier analytics.js

--- analytics.js
+++
@@ -1,2 +0,0 @@
-// Counter.dev Analytics
-if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"{{ site.counter_dev }}",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");
