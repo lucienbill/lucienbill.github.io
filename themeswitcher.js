e19c6a3c5b5780fe8de076684de76befbd6c1e5d
Lucien Bill <lucien.bill@gmail.com>
2021-12-20 21:15:12 +0100

    slightly nicer theme - dark/light mode transition animation - nav is a real nav - small css changes

--- themeswitcher.js
+++ themeswitcher.js
@@ -2,27 +2,33 @@
 const themeSwitcher = document.getElementById("themeSwitcher")
 const page = document.getElementsByTagName("html")[0];
 let currentTheme = localStorage.getItem("data-theme") || "default";
+const lightLogo = "🔆"
+const darkLogo = "🌒"
 
 if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && currentTheme=="default") || currentTheme=="dark") {
   // dark mode
-  themeSwitcher.innerText = "🌒"
-  currentTheme = "dark"
-  page.setAttribute("data-theme",currentTheme);
+  themeSwitcher.innerText = lightLogo
+  switchTheme(true)
 } else {
   //light mode
-  themeSwitcher.innerText = "💡"
-  currentTheme = "light"
-  page.setAttribute("data-theme",currentTheme);
+  themeSwitcher.innerText = darkLogo
+  switchTheme(true)
 }
 
-themeSwitcher.addEventListener("click", () =>{
-    if (themeSwitcher.innerText == "💡") {
-        themeSwitcher.innerText = "🌒"
+themeSwitcher.addEventListener("click", () => {switchTheme()})
+
+function switchTheme(notranstion=false){
+    if (!notranstion){
+        document.querySelector(':root').style.setProperty('--themeswitchtransition', '0.25s');
+    }
+
+    if (themeSwitcher.innerText == lightLogo) {
+        themeSwitcher.innerText = darkLogo
         currentTheme = "dark"
     } else {
-        themeSwitcher.innerText = "💡"
+        themeSwitcher.innerText = lightLogo
         currentTheme = "light"
     }
     page.setAttribute("data-theme", currentTheme);
     localStorage.setItem("data-theme", currentTheme);
-})
+}
