1a8b1778abd322103714fc7ae498f4d9566bb353
Lucien Bill <lucien.bill@gmail.com>
2021-12-31 15:43:46 +0100

    add anchor.js

---
+++ showAnchor.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    anchors.options.placement = 'right';
+    anchors.add('.post h1, .post h2, .post h3, .post h4, .post h5, .post h6');
+});
