1c276222135a19968b6f8c93a07e0a341775024f
Lucien Bill <lucien.bill@gmail.com>
2022-07-27 15:16:59 +0000

    fix: put anchors where they need to be - on blog posts: no more on h1 - new: anchors on "pages" too

--- showAnchor.js
+++ showAnchor.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     anchors.options.placement = 'right';
-    anchors.add('.post h1, .post h2, .post h3, .post h4, .post h5, .post h6');
+    anchors.add('.post h2, .post h3, .post h4, .post h5, .post h6');
+    anchors.add('.page h1, .page h2, .page h3, .page h4, .page h5');
 });
