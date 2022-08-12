9c2e0ee2a82091cb3b38d4cd2cbe921f2932f234
Lucien Bill <lucien.bill@gmail.com>
2022-08-12 16:23:06 +0000

    random snippet : error handling

--- getRandomNewsSnippet.js
+++ getRandomNewsSnippet.js
@@ -30,19 +30,34 @@ async function displaySnippet(fetchkey=null){
     } else {
         response = await fetch(workerUrl)
     }
-    const key = response.headers.get("key")
-    let footer = ""
-    if (key != null){
-        footer = `<hr><p>Cet extrait vous plait ? <a title="lien vers cet\
- extrait" href="${location.toString().split("?")[0]}?key=${key}" target:\
- blank>Partagez-le</a> !<br>Vous pouvez aussi cliquer <a onclick=\
- 'displaySnippet()' title='afficher un autre extrait' href='#'>ici</a> pour\
-  afficher un autre extrait.<p>`
-    }
-    document.getElementById("dynamicContent").innerHTML = pageInfo + await response.json() + footer
 
-    setRandomElementColor();
-    setColorHoverListener();
+    if (response.ok){
+        const key = response.headers.get("key")
+        let footer = ""
+        if (key != null){
+            const newUrl = `${location.toString().replace("#", "").split("?")[0]}?key=${key}`
+            footer = `<hr><p>Cet extrait vous plait ? <a title="lien vers cet\
+extrait" href="${newUrl}" target:\
+blank>Partagez-le</a> !<br>Vous pouvez aussi cliquer <a onclick=\
+'displaySnippet()' title='afficher un autre extrait (aléatoire)' href='#'>ici</a> pour\
+afficher un autre extrait.<p>`
+        }
+        document.getElementById("dynamicContent").innerHTML = pageInfo + await response.json() + footer
+    
+        setRandomElementColor();
+        setColorHoverListener();
+    } else {
+        let message = ""
+        const title = `<h2>Erreur ${response.status}</h2>`
+        const footer = `<hr><p>Retentez votre chance en cliquant <a onclick=\
+        'displaySnippet()' title='afficher un autre extrait (aléatoire)' href='#'>ici</a><p>`
+        if(response.status == 404){
+            message = `<p>Donnée non trouvée (il n'existe pas d'entrée pour la clé ${fetchkey}).<p>`
+        } else {
+            message = `<p>Erreur lors du chargement de données :<p><blockquote>${response.statusText}</blockquote>`
+        }
+        document.getElementById("dynamicContent").innerHTML = title + message + footer
+    }
 }
 
 displaySnippet(findGetParameter("key"))
